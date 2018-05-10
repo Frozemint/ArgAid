95cf16d2f2326c962cc1ff3ccf1062626d06244f
Nami <nami5504@gmail.com>
2018-05-10 11:35:05 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class TextDecode extends React.Component{
+
+	render(){
+		return (
+			<h1>ARG Aid</h1>
+			);
+	}
+}
+
+
+
+ReactDOM.render(<TextDecode/>, document.getElementById('root'));
