1b0f8bfab09d089e071331ffbb63f222d1ac8f65
Nami <nami5504@gmail.com>
2018-05-10 14:11:00 -0700

    added working textfields...

--- src/index.js
+++ src/index.js
@@ -3,14 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class TextDecode extends React.Component{
-
+	constructor(props){
+		super(props);
+		this.state ={
+			string: "test",
+		};
+	}
+	fillLHSTextfield(i){
+		return <LHSField value={i}/>;
+	}
+	fillRHSTextfield(i){
+		return <RHSField value={i}/>;
+	}
 	render(){
 		return (
 			<div>
 			<h1>ARG Aid</h1>
-			<textarea/>
-			<textarea/>
+			{this.fillLHSTextfield(this.state.string)}
+			{this.fillRHSTextfield(this.state.string)}
 			</div>
+			);
+	}
+
+}
+
+class LHSField extends React.Component{
+	render(){
+		return (
+			<textarea placeholder="String to decode...">
+			{this.props.value}
+			</textarea>
+			);
+	}
+}
+
+class RHSField extends React.Component{
+	render(){
+		return (
+			<textarea placeholder="Result...">
+			{this.props.value}
+			</textarea>
 			);
 	}
 }
