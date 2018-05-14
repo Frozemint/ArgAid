76aa4c6ebdea212b6c1968144ac915fa035d90da
Nami <nami5504@gmail.com>
2018-05-14 15:47:01 -0700

    refactored code completely

--- src/index.js
+++ src/index.js
@@ -8,29 +8,57 @@ class TextDecode extends React.Component{
 		super(props);
 		this.state = {
 			string: "",
+			inputBase: 2,
+			outputBase: 10
 		}
 	}
 	handleInput = (event) =>{
 		this.setState({
-			string: event.target.value
+			string: parseInt(event.target.value, this.state.inputBase).toString(this.state.outputBase)
 		});
+	}
+	handleClick = (event) =>{
+
+		if (event.target.name === "InputDropdown"){
+				this.setState({
+					inputBase: event.target.value
+				});
+			} else {
+				this.setState({
+					outputBase: event.target.value
+				});
+			}
 	}
 	render(){
 		return (
-			<div>
-				<TextBoxes name="Input" onChange={this.handleInput} string={this.state.string}/>
-				<TextBoxes name="Output" onChange={this.handleInput} string={this.state.string}/>
+			<div id="Main">
+				<h1>ARG Aid</h1>
+				<br></br>
+				<div id="LHSDiv">
+					<textarea onChange={this.handleInput} string={this.state.string}/>
+					<br/>
+					Interpret input as: <DropDown name="InputDropdown" value={this.state.inputBase} onChange={this.handleClick}/>
+				</div>
+				<div id="RHSDiv">
+					<textarea disabled value={this.state.string}/>
+					<br/>
+					Output as: <DropDown name="OutputDropdown" value={this.state.outputBase} onChange={this.handleClick}/>
+				</div>
 			</div>
 			);
 	}
 
 }
 
-class TextBoxes extends React.Component{
+class DropDown extends React.Component{
 	render(){
 		return (
-			<textarea name={this.props.name} placeholder={this.props.name + "..."} onChange={this.props.onChange}>{this.props.string}</textarea>
-			);
+			<select name={this.props.name} value={this.props.value} onChange={this.props.onChange}>
+						<option value="2">Base 2 (Binary) number</option>
+						<option value="8">Base 8 (Octary) number</option>
+						<option value="10">Base 10 (Decimal) number</option>
+					</select>
+					);
 	}
 }
 
