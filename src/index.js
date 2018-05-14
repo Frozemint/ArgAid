0337f798b44970a3b56b4e2fcb1d980a77125eaf
Nami <nami5504@gmail.com>
2018-05-14 15:23:33 -0700

    the way things should've been done

--- src/index.js
+++ src/index.js
@@ -2,70 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Decode(props){
-	return <textarea placeholder="Result..." value={props.string}></textarea>;
-}
 
 class TextDecode extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
 			string: "",
-			subjectBase: 2,
-			resultBase: 10
-		};
+		}
 		this.handleInput = this.handleInput.bind(this);
 	}
-	getSubjectBase = (event) => { //subject is the string to decode
+	handleInput = (event) =>{
 		this.setState({
-			subjectBase: event.target.value,
-		});
-	}
-	getResultBase = (event) => {
-		this.setState({
-			resultBase: event.target.value,
-		});
-	}
-	handleInput = (event) => {
-		this.setState({
-			string: parseInt(event.target.value, this.state.subjectBase).toString(this.state.resultBase),
+			string: event.target.value
 		});
 	}
 	render(){
 		return (
-			<div id="Main">
-			<h1>ARG Aid</h1>
-			<br></br>
-			<div id="LHSDiv">
-				<textarea placeholder="Number to decode..." onChange={this.handleInput}>
-				{this.state.string}
-				</textarea>
-				<br></br>
-				Interpret number As: <select 
-					value={this.state.subjectBase}
-					onChange={this.getSubjectBase}
-					>
-					<option value="2">Base 2 (Binary)</option>
-					<option value="8">Base 8 (Octary)</option>
-					<option value="10">Base 10 (Decimal)</option>
-				</select>
-			</div>
-			<div id="RHSDiv">
-				<Decode string={this.state.string}/>
-				<br></br>
-				and convert to: <select
-					value="10"
-					onChange={this.getResultBase}
-					>
-					<option value="2">Base 2 (Binary)</option>
-					<option value="8">Base 8 (Octary)</option>
-					<option value="10">Base 10 (Decimal)</option>
-				</select>
-			</div>
+			<div>
+				<TextBoxes onChange={this.handleInput} string={this.state.string}/>
 			</div>
 			);
 	}
 
+}
+
+class TextBoxes extends React.Component{
+	render(){
+		return (
+			<textarea placeholder="String to Decode" onChange={this.props.onChange}>{this.props.string}</textarea>
+			);
+	}
 }
 
 
